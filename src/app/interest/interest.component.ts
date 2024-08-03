276d0fe91a7cfb4527fada2f9b9c842e8c6b6a0c
caiohxp <caiocesar.ccomp@gmail.com>
2024-08-03 02:38:58 -0300

    fix: interewst and project components

--- src/app/interest/interest.component.ts
+++ src/app/interest/interest.component.ts
@@ -7,7 +7,10 @@ import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
   styleUrls: ['./interest.component.css']
 })
 export class InterestComponent {
-  tiposEmpreendimento = ['Residencial', 'Comercial', 'Industrial'];
+  tiposEmpreendimento = ['NATTO BUENO DESIGN', 'OPUS | TERRA DA GRAMA', 'SENSE KASA DESIGN', 
+    'NURBAN ECOBUSINESS DESIGN', 'ALT-65', 'ORBY FRAMBOYANT', 'MIRANTE DO PARQUE', 'ARTE SQUARE', 
+    'VERSAT, ELEV JUNDIAÍ DESIGN', 'NEAR EASY STYLE', 'LIV URBAN MARISTA', 'BLISS ENJOY LIFE', 'DUO SKY GARDEN', 
+    'TALK MARISTA | R.146', 'ELEV MARISTA DESIGN', 'HORIZONTE FLAMBOYANT', 'NOVA ALIANÇA CONDOMÍNIO PREMIUM'];
   formData = {
     tipoEmpreendimento: '',
     nome: '',
@@ -27,6 +30,6 @@ export class InterestComponent {
 
 
   onSubmit() {
-    
+
   }
 }
